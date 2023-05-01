865935423392dea70e0dfe233dc926b2cb15da22
Owen Tuleja <otuleja@Owens-MacBook-Pro.local>
2023-05-01 13:00:45 +0200

    wip

--- server.js
+++ server.js
@@ -9,34 +9,35 @@ var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, 'build')));
 
 function createJWT(user) {
-  return jwt.sign({user}, process.env.SECRET, {expiresIn: '24h'});
+  return jwt.sign({ user }, process.env.SECRET, { expiresIn: '24h' });
 }
 
 app.post('/api/users', function (req, res) {
-    console.log('Here')
-    console.log(req.body)
-    const newUser = new User(req.body)
-    newUser.save().then((user) => {
-      console.log(user)
-         //* creating a new jwt
-         const token = createJWT(user);
-         console.log(token)
-      res.json({token})
-    })
+  console.log('Here')
+  console.log(req.body)
+  const newUser = new User(req.body)
+  newUser.save().then((user) => {
+    console.log(user)
+    //* creating a new jwt
+    const token = createJWT(user);
+    console.log(token)
+    res.json({ token })
+  })
 
 })
 
 app.listen(3001, () => {
-    console.log('Server running...')
-    mongoose.set('strictQuery', true)
-    // connect to mongodDB
-    mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
-    mongoose.connection.once('open', () => {
-        console.log('Connected to MongoDB!')
-    })
+  console.log('Server running...')
+  mongoose.set('strictQuery', true)
+  // connect to mongodDB
+  mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB!')
+  })
 })
